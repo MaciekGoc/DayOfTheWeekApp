123f48bfd0d103c8a030551ccb6bbca1f81fb5d2
MaciekGoc <mggocu@gmail.com>
2022-02-16 23:10:55 +0100

    Add project files.

---
+++ DayOfTheWeekApp/DayGuesser.cs
@@ -0,0 +1,56 @@
+﻿using DayOfTheWeekApp.Core;
+using System;
+
+namespace DayOfTheWeekApp
+{
+    public class DayGuesser
+    {
+        public DayCalculator Calculator { get; set; }
+
+        public DateTimeOffset UserDateOfBirth { get; set; }
+        public DayOfTheWeek UserDayOfTheWeek { get; set; }
+
+        public void IntroduceTheApplication()
+        {
+            Console.WriteLine("Witaj, jestem botem, który potrafi wyliczać dzień tygodnia na podstawie Twojej daty urodzenia.");
+
+            Calculator = new DayCalculator();
+        }
+        
+
+        public void AskUserForTheirDateOfBirth()
+        {
+            Console.WriteLine("Podaj proszę swoją datę urodzenia:");
+
+            var userDate = Console.ReadLine();
+
+            var succeded = DateTimeOffset.TryParse(userDate, out var date);
+
+            if (succeded)
+            {
+                UserDateOfBirth = date;
+                return;
+            }
+
+            Console.WriteLine("Format daty nieprawidłowy. Proszę go podać w dd/mm/yyyy");
+            AskUserForTheirDateOfBirth();
+        }
+
+        public void CalculateDayOfTheWeek()
+        {
+            if (UserDateOfBirth == null)
+            {
+                Console.WriteLine("Próbowano obliczyć dzień tygodnia bez podania daty urodzenia");
+                return;
+            }
+
+            UserDayOfTheWeek = Calculator.CalculateDayOfTheWeek(UserDateOfBirth);
+        }
+
+        public void PrintDayOfTheWeek()
+        {
+            Console.WriteLine("Obliczony dzień tygodnia to: " + UserDayOfTheWeek.ToPolishString());
+            Console.WriteLine("Dziękuję za kooperację!");
+        }
+    }
+}
---
+++ DayOfTheWeekApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using DayOfTheWeekApp.Core;
+using System;
+
+namespace DayOfTheWeekApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var guesser = new DayGuesser();
+            guesser.IntroduceTheApplication();
+            guesser.AskUserForTheirDateOfBirth();
+            guesser.CalculateDayOfTheWeek();
+            guesser.PrintDayOfTheWeek();
+
+
+        }
+    }
+}
